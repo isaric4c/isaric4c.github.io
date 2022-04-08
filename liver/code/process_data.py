34826fead5de4a9b0c81816e87e59d42f26111d1
StevenRKerr <stevenker2@gmail.com>
2022-04-08 15:37:29 +0100

    Updated AST_ALT_counter in liver directory

--- liver/code/process_data.py
+++ liver/code/process_data.py
@@ -6,9 +6,6 @@ Created on Wed Apr  6 09:09:01 2022
 """
 
 import pandas as pd
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -17,14 +14,15 @@ args = parser.parse_args()
 
 #### Lookup dataframes
 
-# Look ups go here. Needs to be updated
+# Look ups for upper limit or normal range. 
+# Just now its 50 in all age groups for AST and ALT.
 AST_lookup = pd.DataFrame( {'age_group': ['<3wks', '3wks-5y', '6-10y', '11-16y', 
                                       '17-30y', '31-50y', '51-70y', '>70y'],
-                        'AST_upper_limit': np.arange(8)*100  }  )
+                        'AST_upper_limit': [50]*8  }  )
 
 ALT_lookup = pd.DataFrame( {'age_group': ['<3wks', '3wks-5y', '6-10y', '11-16y', 
                                       '17-30y', '31-50y', '51-70y', '>70y'],
-                        'ALT_upper_limit': np.arange(8)*100  }  )
+                        'ALT_upper_limit': [50]*8  }  )
 
 #### Functions
 
@@ -67,9 +65,11 @@ def pre_process_data(input_filepath):
     # Add flags for above threshold value
     df['AST_2x_normal'] = df['AST'] > 2* df['AST_upper_limit']
     df['ALT_2x_normal'] = df['ALT'] > 2* df['ALT_upper_limit']
-    
+        
     return df
-    
+
+
+
 
 def create_AST_ALT_counts(df, output_filepath):    
     '''
@@ -86,7 +86,29 @@ def create_AST_ALT_counts(df, output_filepath):
     df_out : dataframe with counts of unique patients by measurement, month and age group
 
     '''
-     
+    
+    # Remove any samples that are taken after the first sample that has
+    # elevated AST or ALT
+    df_list = []
+        
+    for ID in df.patient_id.unique():
+        
+        df_patient = df[ (df['patient_id'] == ID)]
+        
+        elevated = df_patient[ df_patient['AST_2x_normal'] | 
+                               df_patient['ALT_2x_normal']]
+        
+        if len(elevated) > 0:
+            
+            date_first_elevated = elevated['sample_date'].min()
+            
+            df_patient = df_patient[ df_patient['sample_date'] <= date_first_elevated] 
+            
+        df_list.append( df_patient)
+        
+    df = pd.concat(df_list)    
+    
+       
     # Create dataframe that will be filled out and be the final output
     cols = ['count', 'age_group'] + pd.date_range('2018-01-01','2022-03-01', 
               freq='MS').strftime("%b-%Y").tolist()
@@ -107,7 +129,7 @@ def create_AST_ALT_counts(df, output_filepath):
     df_out = df_out.merge(df_sub, how = 'outer')
     
     # All nan are zero counts
-    df_out = df_out.replace(np.nan, 0)
+    df_out = df_out.fillna(0)
   
     # Sort columns
     df_out = df_out.reindex(columns=cols)
@@ -162,15 +184,31 @@ def sub_process(df, measure, elevated):
     # Merge with lookup so all age groups are present
     # Rest index
     df.insert(0, 'age_group', df.index)
-    df.index = np.arange(len(df))
+    df = df.reset_index(drop = True)
     df = df.merge(AST_lookup['age_group'], how = 'right')   
     df.insert(0, 'count', col_name)
    
     return df
 
 
+
 def create_first_AST_ALT_values(df, output_filepath):
-    
+    '''
+
+    Parameters
+    ----------
+    df : TYPE
+        DESCRIPTION.
+    output_filepath : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    df_out : TYPE
+        DESCRIPTION.
+
+    '''
+          
     df = df.sort_values( by = ['sample_date'])
       
     # AST
@@ -179,18 +217,17 @@ def create_first_AST_ALT_values(df, output_filepath):
     # Get first elevated AST measurement
     df_elevated = df[df[col_name] == True].groupby(['patient_id'], as_index = False).first()
     df_elevated = df_elevated.drop(['ALT', 'ALT_2x_normal', 'AST_upper_limit', 'ALT_upper_limit'], axis = 1)
-    df_elevated = df_elevated.rename(columns={"sample_date": "AST_sample_date",
-                                              "month": "AST_sample_month"})
+
           
     # Remove anyone who has an elevated AST measurement. Get their first AST measurement
     df_normal = df[~df['patient_id'].isin( df_elevated.patient_id.unique() )]
     
     df_normal = df_normal.groupby(['patient_id'], as_index = False).first()
     df_normal = df_normal.drop(['ALT', 'ALT_2x_normal', 'AST_upper_limit', 'ALT_upper_limit'], axis = 1)
-    df_normal = df_normal.rename(columns={"sample_date": "AST_sample_date",
-                                              "month": "AST_sample_month"})
     
-    df_AST = df_elevated.merge(df_normal, how = 'outer')
+    df_AST = df_elevated.merge(df_normal, how = 'outer').rename(columns={
+                                              "month": "AST_sample_month",
+                                              "age_group":"AST_age_group"}).drop(['sample_date', 'age', 'DOB'], axis = 1)
     
     
     # ALT
@@ -199,30 +236,26 @@ def create_first_AST_ALT_values(df, output_filepath):
     # Get first elevated ALT measurement
     df_elevated = df[df[col_name] == True].groupby(['patient_id'], as_index = False).first()
     df_elevated = df_elevated.drop(['AST', 'AST_2x_normal', 'AST_upper_limit', 'ALT_upper_limit'], axis = 1)
-    df_elevated = df_elevated.rename(columns={"sample_date": "ALT_sample_date",
-                                              "month": "ALT_sample_month"})
+
           
     # Remove anyone who has an elevated AST measurement. Get their first AST measurement
     df_normal = df[~df['patient_id'].isin( df_elevated.patient_id.unique() )]
     
     df_normal = df_normal.groupby(['patient_id'], as_index = False).first()
     df_normal = df_normal.drop(['AST', 'AST_2x_normal', 'AST_upper_limit', 'ALT_upper_limit'], axis = 1)
-    df_normal = df_normal.rename(columns={"sample_date": "ALT_sample_date",
-                                              "month": "ALT_sample_month"})
-    
-    df_ALT = df_elevated.merge(df_normal, how = 'outer')
-    
-    
-    
-    df_out = df_AST.merge(df_ALT, how = 'outer')
+
+    df_ALT = df_elevated.merge(df_normal, how = 'outer').rename(columns={
+                                              "month": "ALT_sample_month",
+                                              "age_group":"ALT_age_group"}).drop(['sample_date','age', 'DOB'], axis = 1)
+       
+    df_out = df_AST.merge(df_ALT, how = 'outer', on = ['patient_id'])
                
     # Remove identifying columns
-    df_out = df_out.drop(['patient_id', 'DOB', 'AST_sample_date', 'age',
-                          'ALT_sample_date'], axis = 1)
+    df_out = df_out.drop(['patient_id'], axis = 1)
     
     # reorder columns
-    df_out = df_out[ ['age_group', 'AST_sample_month', 'AST', 'AST_2x_normal',
-                                   'ALT_sample_month', 'ALT', 'ALT_2x_normal']]
+    df_out = df_out[ ['AST_age_group', 'AST_sample_month', 'AST', 'AST_2x_normal',
+                      'ALT_age_group', 'ALT_sample_month', 'ALT', 'ALT_2x_normal']]
     
     # Save out
     df_out.to_csv(output_filepath, index=False)
@@ -230,46 +263,6 @@ def create_first_AST_ALT_values(df, output_filepath):
     return df_out
 
 
-def create_plots(AST_ALT_counts, output_folderpath):
-    '''
-
-    Parameters
-    ----------
-    AST_ALT_counts : Counts of AST, ALT measurements by age group and month
-    
-    output_folderpath : Folder where graph pngs will be saved
-
-    Returns
-    -------
-
-    '''
-          
-    # X axis ticks
-    x = pd.date_range('2018-01-01','2022-03-01', 
-              freq='MS').strftime("%b-%Y").tolist()
-            
-    for measure in ['AST_all_tests', 'ALT_all_tests', 'AST_2x_normal', 'ALT_2x_normal']:
-
-        df = AST_ALT_counts[ AST_ALT_counts['count'] == measure].reset_index(drop = True)
-        
-        plt.figure(figsize=(35,20))
-        
-        for i in range(len(df)):
-         
-            y = df.loc[i, x]
-            
-            # plot lines
-            plt.plot(x, y, label = df.loc[i, 'age_group'])
-        
-        plt.xticks(rotation = 320)
-        plt.legend()
-        plt.title(measure)
-        matplotlib.rc('font', size = 15)
-        
-        plt.savefig(output_folderpath + '/' + measure + '.png')
-        
-        return
-    
   
 ####  
  
@@ -277,9 +270,7 @@ df = pre_process_data(args.filename)
 
 AST_ALT_counts = create_AST_ALT_counts(df, 'summary_table.csv')
 first_AST_ALT_values = create_first_AST_ALT_values(df, 'first_AST_ALT_values.csv')
-
-
-create_plots(AST_ALT_counts, '.')
+ 
 
 
 
